3e1cafb564320946e0e774ff2c7e1a4d85794f31
grxya <gariba.dadashova@gmail.com>
2023-12-09 01:49:33 +0400

    new HW

---
+++ State/Computer.cs
@@ -0,0 +1,45 @@
+﻿using State.States.Classes;
+using State.States.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Computer
+    {
+        public IComputerState State { get; set; }
+
+        public Computer(IComputerState state)
+        {
+            State = state;
+        }
+
+        public void PowerOn()
+        {
+            State.PowerOn(this);
+        }
+
+        public void PowerOff()
+        {
+            State.PowerOff(this);
+        }
+
+        public void Hibernate()
+        {
+            State.Hibernate(this);
+        }
+
+        public void Sleep()
+        {
+            State.Sleep(this);
+        }
+
+        public void PlayGame()
+        {
+            State.PlayGame(this);
+        }
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,9 @@
+﻿using State;
+using State.States.Classes;
+
+Computer computer = new Computer(new OffState());
+
+computer.Sleep(); computer.Hibernate(); computer.PlayGame(); computer.PowerOn(); Console.WriteLine();
+computer.PowerOn(); computer.Sleep(); computer.Sleep(); computer.Hibernate(); computer.PlayGame(); computer.PowerOn(); Console.WriteLine();
+computer.Hibernate(); computer.Hibernate(); computer.PlayGame(); computer.Sleep(); computer.PowerOff(); computer.PowerOff(); computer.PlayGame();
+
