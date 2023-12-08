ce474bde669e1fb9ff20f83c04f8784956233df4
grxya <gariba.dadashova@gmail.com>
2023-12-09 01:48:08 +0400

    update

--- State/ComputerState/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using State;
-using State.States.Classes;
-
-Computer computer = new Computer(new OffState());
-
-computer.Sleep(); computer.Hibernate(); computer.PlayGame(); computer.PowerOn(); Console.WriteLine();
-computer.PowerOn(); computer.Sleep(); computer.Sleep(); computer.Hibernate(); computer.PlayGame(); computer.PowerOn(); Console.WriteLine();
-computer.Hibernate(); computer.Hibernate(); computer.PlayGame(); computer.Sleep(); computer.PowerOff(); computer.PowerOff(); computer.PlayGame();
-
