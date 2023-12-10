84679843644c82fa6c1bf8c78fc96ed75810b62f
grxya <gariba.dadashova@gmail.com>
2023-12-10 04:01:51 +0400

    last HW

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -5,7 +5,7 @@ using Proxy.Entities.Classes;
 User user = new User("grxya", ConfidentialityLevel.Internal);
 Document document = new Document("Secret Document", ConfidentialityLevel.Confidential);
 
-user.OpenDocument(document);Console.WriteLine();
+user.OpenDocument(document); Console.WriteLine();
 user.OpenDocument(new DocumentProxy(document)); Console.WriteLine();
 
 document.Confidentiality = ConfidentialityLevel.Public;
