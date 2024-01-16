1b6772590303464a9dffd3141a365abe5ada060f
grxya <gariba.dadashova@gmail.com>
2024-01-16 12:20:31 +0400

    new HW

---
+++ HW2/MainWindow.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace HW2
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tablesBox = new System.Windows.Forms.ComboBox();
+            this.dbDataGridView = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.getAllDatabtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tablesBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dbDataGridView, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.555555F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.44444F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(908, 557);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tablesBox
+            // 
+            this.tablesBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tablesBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.tablesBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.tablesBox.FormattingEnabled = true;
+            this.tablesBox.Location = new System.Drawing.Point(3, 7);
+            this.tablesBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tablesBox.Name = "tablesBox";
+            this.tablesBox.Size = new System.Drawing.Size(448, 28);
+            this.tablesBox.TabIndex = 0;
+            // 
+            // dbDataGridView
+            // 
+            this.dbDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dbDataGridView, 2);
+            this.dbDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbDataGridView.Location = new System.Drawing.Point(3, 46);
+            this.dbDataGridView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dbDataGridView.Name = "dbDataGridView";
+            this.dbDataGridView.RowHeadersWidth = 51;
+            this.dbDataGridView.RowTemplate.Height = 25;
+            this.dbDataGridView.Size = new System.Drawing.Size(902, 507);
+            this.dbDataGridView.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.deleteBtn);
+            this.panel1.Controls.Add(this.SaveBtn);
+            this.panel1.Controls.Add(this.getAllDatabtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(457, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(448, 34);
+            this.panel1.TabIndex = 2;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.deleteBtn.Location = new System.Drawing.Point(204, 0);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(86, 34);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SaveBtn.Location = new System.Drawing.Point(118, 0);
+            this.SaveBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(86, 34);
+            this.SaveBtn.TabIndex = 1;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // getAllDatabtn
+            // 
+            this.getAllDatabtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.getAllDatabtn.Location = new System.Drawing.Point(0, 0);
+            this.getAllDatabtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.getAllDatabtn.Name = "getAllDatabtn";
+            this.getAllDatabtn.Size = new System.Drawing.Size(118, 34);
+            this.getAllDatabtn.TabIndex = 0;
+            this.getAllDatabtn.Text = "Get All Data";
+            this.getAllDatabtn.UseVisualStyleBackColor = true;
+            this.getAllDatabtn.Click += new System.EventHandler(this.getAllDatabtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 557);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private ComboBox tablesBox;
+        private DataGridView dbDataGridView;
+        private Panel panel1;
+        private Button deleteBtn;
+        private Button SaveBtn;
+        private Button getAllDatabtn;
+    }
+}
---
+++ HW2/MainWindow.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2
+{
+    public partial class MainWindow : Form
+    {
+        private SqlConnection conn;
+        private SqlDataAdapter adapter;
+        private DataTable dt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets<MainWindow>();
+            var config = builder.Build();
+            var connectionString = config["DefaultConnection"];
+
+            conn = new SqlConnection(connectionString);
+
+            conn.Open();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            using SqlCommand cmd = new("select [name] from sys.tables", conn);
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                tablesBox.Items.Add(reader["name"]);
+            }
+        }
+
+        private void getAllDatabtn_Click(object sender, EventArgs e)
+        {
+            if (tablesBox.SelectedItem == null)
+            {
+                MessageBox.Show("Select table first", "ERROR!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            adapter = new($"select * from {tablesBox.SelectedItem}", conn);
+
+            dt = new();
+            adapter.Fill(dt);
+
+            dbDataGridView.DataSource = dt;
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            using SqlCommandBuilder builder = new(adapter);
+
+            adapter.Update(dt);
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (dbDataGridView.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("Select row first", "ERROR!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // удаление только из памяти, то есть для сохранения в бд требует обновление через адаптер (кликнуть save, как и с другими изменения)
+            foreach (DataGridViewRow row in dbDataGridView.SelectedRows)
+            {
+                dt.Rows[row.Index].Delete();
+            }
+
+            // удаление из самой бд
+            /* foreach (DataGridViewRow row in dbDataGridView.SelectedRows)
+               {
+                   adapter.DeleteCommand = new($"delete from {tablesBox.SelectedItem} where Id={row.Cells[0].Value}", conn);
+                   adapter.DeleteCommand.ExecuteNonQuery();
+               }*/
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            conn.Close();
+        }
+    }
+}
