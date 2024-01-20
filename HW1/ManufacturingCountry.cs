8ae30e714181ead93cff6fc72469506ece533275
grxya <gariba.dadashova@gmail.com>
2024-01-21 00:02:58 +0400

    new HW

---
+++ HW1/ManufacturingCountry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW1
+{
+    public class ManufacturingCountry
+    {
+        public ManufacturingCountry()
+        {
+            Sellers = new HashSet<Seller>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } 
+
+        public ICollection<Seller> Sellers { get; set; }
+    }
+}
