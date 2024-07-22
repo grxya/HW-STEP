b0361b71f0286371024824916dd464acc6a2ee02
grxya <gariba.dadashova@gmail.com>
2024-07-23 00:28:47 +0400

    small update

--- HW1/HW1.1/TaskManagementDB-1.sql
+++ HW1/HW1.1/TaskManagementDB-1.sql
@@ -11,9 +11,6 @@ create table Tasks (
 );
 go;
 
-/*dotnet ef dbcontext scaffold "Server=localhost;Database=TaskManagementDB;User Id=admin;Password=admin;Trust Server Certificate=true;" Microsoft.EntityFrameworkCore.SqlServer
-*/
-
 insert into Tasks (Name, Description, Deadline)
     values
     ('First Task', 'Description for first task', '2024-07-31'),
