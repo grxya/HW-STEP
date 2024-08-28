733419c54e2dab6b7576a7f999fc39e2375f0c57
grxya <gariba.dadashova@gmail.com>
2024-08-28 16:15:41 +0400

    new HW

---
+++ HW1/AutoResetEvent/Program.cs
@@ -0,0 +1,23 @@
+﻿using var autoResetEvent = new AutoResetEvent(false);
+
+var mutex = new Mutex();
+
+for (int i = 0; i < 5; i++)
+{
+    ThreadPool.QueueUserWorkItem(
+    (p) =>
+    {
+        mutex.WaitOne();
+        autoResetEvent.Set();
+
+        Console.WriteLine($"Is ThreadPool Thread: {Thread.CurrentThread.IsThreadPoolThread}");
+        Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+        mutex.ReleaseMutex();
+    });
+}
+
+Thread.Sleep(1000);
+autoResetEvent.WaitOne();
+
+Console.WriteLine("End of main");
---
+++ HW1/ManualResetEvent/Program.cs
@@ -0,0 +1,23 @@
+﻿using var manualResetEvent = new ManualResetEvent(false);
+
+var mutex = new Mutex();
+
+for (int i = 0; i < 5; i++)
+{
+    ThreadPool.QueueUserWorkItem(
+    (p) =>
+    {
+        mutex.WaitOne();
+        manualResetEvent.Set();
+
+        Console.WriteLine($"Is ThreadPool Thread: {Thread.CurrentThread.IsThreadPoolThread}");
+        Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+        mutex.ReleaseMutex();
+    });
+}
+
+Thread.Sleep(1000);
+manualResetEvent.WaitOne();
+
+Console.WriteLine("End of main");
