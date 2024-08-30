8ae68505a70ad15634cb190a102c63386709f603
grxya <gariba.dadashova@gmail.com>
2024-08-30 16:26:00 +0400

    HW3 task1-3

---
+++ HW3/TextAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextAnalyzer
+{
+    public partial class MainWindow : Window
+    {
+        private readonly TextReportService _textReportService = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await ReportGenerator();
+
+            using StreamWriter sw = new(new FileStream("report.txt", FileMode.OpenOrCreate));
+
+            sw.WriteAsync(result);
+        }
+
+        private async void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReportTextBox.Text = await ReportGenerator();
+        }
+
+        private async Task<string> ReportGenerator()
+        {
+            if (FullCheckBox.IsChecked == true)
+            {
+                return await _textReportService.FullReport(MainTextBox.Text);
+            }
+
+            string result = "";
+
+            if (SentencesCheckBox.IsChecked == true)
+            {
+                result += await _textReportService.NumberOfSentences(MainTextBox.Text);
+            }
+            if (SymbolsCheckBox.IsChecked == true)
+            {
+                result += await _textReportService.NumberOfSymbols(MainTextBox.Text);
+            }
+            if (WordsCheckBox.IsChecked == true)
+            {
+                result += await _textReportService.NumberOfWords(MainTextBox.Text);
+            }
+            if (ExclamatoryCheckBox.IsChecked == true)
+            {
+                result += await _textReportService.NumberOfExclamatory(MainTextBox.Text);
+            }
+            if (QuestionsCheckBox.IsChecked == true)
+            {
+                result += await _textReportService.NumberOfQuestions(MainTextBox.Text);
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ HW3/TextAnalyzer/TextReportService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    public class TextReportService
+    {
+        public async Task<string> NumberOfSentences(string text)
+        {
+            int result = await Task.Run(() => text.Count(x => x == '.' || x == '!' || x == '?'));
+
+            return $"The number of sentences: {result}\n";
+        }
+
+        public async Task<string> NumberOfSymbols(string text)
+        {
+            int result = await Task.Run(() => text.Count());
+
+            return $"The number of symbols: {result}\n";
+        }
+
+        public async Task<string> NumberOfWords(string text)
+        {
+            var words = await Task.Run(() => text.Split(' '));
+
+            return $"The number of words: {words.Length}\n";
+        }
+
+        public async Task<string> NumberOfExclamatory(string text)
+        {
+            int result = await Task.Run(() => text.Count(x => x == '!'));
+
+            return $"The number of exclamatory sentences: {result}\n";
+        }
+
+        public async Task<string> NumberOfQuestions(string text)
+        {
+            int result = await Task.Run(() => text.Count(x => x == '?'));
+
+            return $"The number of questions: {result}\n";
+        }
+
+        public async Task<string> FullReport(string text)
+        {
+            return await NumberOfSentences(text) + await NumberOfSymbols(text) 
+                + await NumberOfWords(text) + await NumberOfExclamatory(text) + await NumberOfQuestions(text);
+        }
+    }
+}
